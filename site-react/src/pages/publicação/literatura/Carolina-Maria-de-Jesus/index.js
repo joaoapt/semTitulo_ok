771fcad50f6f50d4c2bf6024d54537161f9db32c
joaoapt <melojpf@gmail.com>
2023-06-20 07:19:07 -0300

    Fim2

--- site-react/src/pages/publicação/literatura/Carolina-Maria-de-Jesus/index.js
+++ site-react/src/pages/publicação/literatura/Carolina-Maria-de-Jesus/index.js
@@ -8,16 +8,16 @@ export default function Index() {
             <nav><Buscar/></nav>
             <div className='conteudo'>
                 <div>
-                    <h1 className='titulo-texto'>Maria Esther Andion Bueno</h1>
+                    <h1 className='titulo-texto'>Carolina Maria de Jesus</h1>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Maria Esther Andion Bueno, conhecida no exterior como Maria Bueno,  Nascida em 11 de outubro de 1939, São Paulo, São Paulo foi uma tenista brasileira, que atuou nas décadas de 1950, 1960 e 1970, sendo uma das raras tenistas a conquistar títulos em três décadas diferentes.</p>
+                    <p className='sub-texto'>A vida de Carolina Maria de Jesus é uma história de luta, superação e perseverança. Nascida em uma família pobre em Sacramento, Brasil, Carolina enfrentou uma infância marcada pela pobreza e pelas adversidades. Ela viveu em uma favela em São Paulo, onde enfrentou condições precárias de moradia e falta de oportunidades.</p>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Qual o efeito de Maria Esther Bueno? Maria Esther Bueno foi uma importante tenista brasileira que venceu os maiores e mais reconhecidos torneios de tênis do mundo. O objetivo desta pesquisa foi compreender os processos que possibilitaram a Maria Esther Bueno tornar-se uma grande atleta em uma época de baixa inserção das mulheres no esporte competitivo.</p>
+                    <p className='sub-texto'>Carolina começou a escrever em diários encontrados no lixo, registrando suas experiências diárias e expressando seus sentimentos e pensamentos sobre a desigualdade e a injustiça social que testemunhava. Em 1960, seu trabalho foi descoberto por um jornalista e resultou na publicação do livro "Quarto de Despejo: Diário de uma Favelada". A obra, escrita com uma linguagem franca e visceral, se tornou um sucesso internacional, retratando a realidade da pobreza e dando voz aos marginalizados.</p>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Em 1960, ela entrou para a história como a primeira mulher a ganhar o chamado Grand Slam de tênis, ou seja, a conquistar os quatro Majors jogando em duplas num mesmo ano (três com Darlene Hard e um com Christine Truman Janes). Seu nome está no Livro dos Recordes: na final do US Open de 1964, contra a americana Carole Caldwell Graebner, Maria Esther venceu a partida em apenas dezenove minutos. Além disso, sua vitória sobre Margaret Court na final de Wimbledon, em 1964, é considerada por muitos um dos dez jogos mais emocionantes da história do tênis.</p>
+                    <p className='sub-texto'>Apesar de sua ascensão à fama literária, Carolina enfrentou muitos desafios ao longo de sua vida. Ela lutou contra a discriminação racial e as dificuldades financeiras, mas nunca abandonou sua paixão pela escrita. Carolina continuou a produzir obras literárias, embora com menos reconhecimento do que seu primeiro livro. Ela deixou um legado importante como uma das primeiras escritoras negras do Brasil a alcançar reconhecimento nacional e internacional.</p>
                 </div>
             </div>
         </main>
--- site-react/src/pages/publicação/literatura/Montero-Lobato/index.js
+++ site-react/src/pages/publicação/literatura/Montero-Lobato/index.js
@@ -8,16 +8,16 @@ export default function Index() {
             <nav><Buscar/></nav>
             <div className='conteudo'>
                 <div>
-                    <h1 className='titulo-texto'>Maria Esther Andion Bueno</h1>
+                    <h1 className='titulo-texto'>Monteiro Lobato</h1>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Maria Esther Andion Bueno, conhecida no exterior como Maria Bueno,  Nascida em 11 de outubro de 1939, São Paulo, São Paulo foi uma tenista brasileira, que atuou nas décadas de 1950, 1960 e 1970, sendo uma das raras tenistas a conquistar títulos em três décadas diferentes.</p>
+                    <p className='sub-texto'>A vida de Monteiro Lobato foi marcada por sua paixão pela literatura e seu compromisso com a educação. Nascido em Taubaté, Brasil, em 1882, Lobato teve uma infância influenciada pela leitura e pelo contato com livros. Sua imaginação fértil e interesse pela escrita o levaram a se tornar um dos mais renomados escritores brasileiros.</p>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Qual o efeito de Maria Esther Bueno? Maria Esther Bueno foi uma importante tenista brasileira que venceu os maiores e mais reconhecidos torneios de tênis do mundo. O objetivo desta pesquisa foi compreender os processos que possibilitaram a Maria Esther Bueno tornar-se uma grande atleta em uma época de baixa inserção das mulheres no esporte competitivo.</p>
+                    <p className='sub-texto'>Lobato é conhecido principalmente por suas obras infantis, que apresentam personagens cativantes, como a turma do Sítio do Picapau Amarelo. Suas histórias combinam elementos de fantasia e realidade, abordando questões sociais e culturais de forma lúdica e acessível. Lobato tinha como objetivo despertar o interesse das crianças pela leitura e pela cultura brasileira, incentivando a imaginação e a curiosidade.</p>
                 </div>
                 <div  className='sub-lol'>
-                    <p className='sub-texto'>Em 1960, ela entrou para a história como a primeira mulher a ganhar o chamado Grand Slam de tênis, ou seja, a conquistar os quatro Majors jogando em duplas num mesmo ano (três com Darlene Hard e um com Christine Truman Janes). Seu nome está no Livro dos Recordes: na final do US Open de 1964, contra a americana Carole Caldwell Graebner, Maria Esther venceu a partida em apenas dezenove minutos. Além disso, sua vitória sobre Margaret Court na final de Wimbledon, em 1964, é considerada por muitos um dos dez jogos mais emocionantes da história do tênis.</p>
+                    <p className='sub-texto'>Além de seu trabalho como escritor, Monteiro Lobato também teve uma atuação destacada na área da educação. Ele fundou uma editora e criou um projeto pioneiro de literatura infantil para escolas, conhecido como "Biblioteca de Monteiro Lobato". Seu empenho em melhorar a qualidade da educação no Brasil o levou a defender mudanças no sistema educacional e a lutar por uma abordagem mais dinâmica e criativa no ensino.</p>
                 </div>
             </div>
         </main>
